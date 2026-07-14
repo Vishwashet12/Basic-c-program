7e9fc4c91eee7109791f653de4d608ff97d9bf19
Vishwashet12 <shetvishwa8@gmail.com>
2026-07-14 18:49:52 +0530

    Add files via upload

---
+++ area and circumference of circle.c
@@ -0,0 +1,13 @@
+// Leena Metha
+// 2503033111372049
+#include<stdio.h>
+int main(){
+int r,area,circumference;
+printf("Enter radius: ");
+scanf("%d",&r);
+area=3*r*r;
+circumference=2*3*r;
+printf("Area = %d\n",area);
+printf("Circumference = %d",circumference);
+return 0;
+}
