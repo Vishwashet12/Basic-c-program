64eba42d68fb705b125136a2895affbf5aea60af
Vishwashet12 <shetvishwa8@gmail.com>
2026-07-14 18:48:45 +0530

    Add files via upload

---
+++ addition in array.c
@@ -0,0 +1,17 @@
+// Leena Lukesh Metha
+// 2503033111372049
+
+
+#include<stdio.h>
+int main(){
+int a[5]={1,1,1,1,1},b[5]={2,2,2,2,2},c[5],i;
+for(i=0;i<=4;i++){
+c[i]=a[i]+b[i];
+}
+printf("addition of a and b");
+for(i=0;i<=4;i++){
+	printf("%d",c[i]);
+}
+return 0;
+}
+
